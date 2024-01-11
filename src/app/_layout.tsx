0c1f1180cc22a3f0fccafdb6db6c2f6782374739
Michał Bar <9134970+miikebar@users.noreply.github.com>
2024-01-11 16:09:17 +0100

    chore: initial commit

---
+++ src/app/_layout.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+import { Stack } from 'expo-router';
+import '../../assets/global.css';
+
+const RootLayout: FC = () => {
+  return <Stack />;
+};
+
+export default RootLayout;
